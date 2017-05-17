159a81c9eea657440eb6ed61f0b2192703e3c050
bmenn <bmenn@example.com>
2017-05-16 22:14:08 -0400

    Add cmder to configuration for Windows 10

---
+++ cmder/user-profile.sh
@@ -0,0 +1,5 @@
+# use this file to run your own startup commands for msys2 bash'
+
+# To add a new vendor to the path, do something like:
+# export PATH=${CMDER_ROOT}/vendor/whatever:${PATH}
+source $CMDER_ROOT/vendor/mintty-colors-solarized/mintty-solarized-dark.sh
