3a251ac9b5a61a29ddd5fffa72bafe264ac0e4a6
bmenn <bmenn@example.com>
2018-03-11 14:35:23 -0400

    Centralize shell functions

---
+++ sh/functions.sh
@@ -0,0 +1,44 @@
+# Usage: activate
+# Description: activates python virtualenv
+function activate {
+        source venv/bin/activate
+}
+
+# Usage: extract <file>
+# Description: extracts archived files (maybe)
+function extract () {
+    if [ -f $1 ]; then
+        case $1 in
+            *.tar.bz2)  tar -xvf $1        ;;
+            *.tar.gz)   tar -xvf $1        ;;
+            *.bz2)      bzip2 -d $1         ;;
+            *.gz)       gunzip -d $1        ;;
+            *.tar)      tar -xvf $1         ;;
+            *.tgz)      tar -zxvf $1        ;;
+            *.zip)      unzip $1            ;;
+            *.Z)        uncompress $1       ;;
+            *.rar)      unrar x $1            ;;
+            *)          echo "'$1' Error. Please go away" ;;
+        esac
+        else
+            echo "'$1' is not a valid file"
+    fi
+}
+
+# Usage: show-archive <archive>
+# Description: view archive without unpack
+function show-archive() {
+    if [ -f $1 ]; then
+        case $1 in
+            *.tar.bz2)     tar -jtf $1 ;;
+            *.tar.gz)      tar -ztf $1 ;;
+            *.tar)         tar -tf $1  ;;
+            *.tgz)         tar -ztf $1 ;;
+            *.zip)         unzip -l $1 ;;
+            *.rar)         rar vb $1   ;;
+            *)             echo "'$1' Error. Please go away" ;;
+        esac
+        else
+            echo "'$1' is not a valid archive"
+    fi
+}
